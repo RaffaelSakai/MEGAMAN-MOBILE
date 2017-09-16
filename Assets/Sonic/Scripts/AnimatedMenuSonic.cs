5bc7f8bd6f2f20d5fe8bd1c70974c645b29fa164
THIAGO <lemric123@outlook.com>
2017-09-15 23:10:54 -0300

    Alterei a tela de menu principal. A proxima sera a tela de selecao de fases

---
+++ Assets/Sonic/Scripts/AnimatedMenuSonic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedMenuSonic : MonoBehaviour
+{
+
+    int valorAleatorio;
+    Animator anim;
+    public int Limite;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Sonic Idle Impaciente"))
+        {
+            Contador(Limite);
+        }
+    }
+
+    float soma = 0;
+    void Contador(int valorLimite)
+    {
+        soma += Time.deltaTime;
+        print(soma);
+        if (soma >= valorLimite)
+        {
+            soma = 0;
+            int numeroEscolhido = valorAleatorio;
+            valorAleatorio = Random.Range(0, 3);
+            while(valorAleatorio == numeroEscolhido)
+            {
+                valorAleatorio = Random.Range(0, 3);
+            }
+
+            TocaAnimacao(valorAleatorio);
+        }
+    }
+
+    void TocaAnimacao(int numero)
+    {
+        anim.Play("Animacao" + numero, -1, 0f);
+        //anim.Play("Animacao2", -1, 0f);
+    }
+}
