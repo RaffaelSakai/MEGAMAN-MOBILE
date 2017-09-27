d74293e14f97c256db3578d1835a3abb7f98d33f
Raffa <raffaelvidotto@gmail.com>
2017-09-27 17:32:02 -0300

    Texto ja ta feito junto com a animação

---
+++ Assets/Teste.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teste : MonoBehaviour
+{
+    Image Render;
+    public List<Sprite> sprites;
+    float index = 0;
+    public int velocidade;
+    void Start()
+    {
+        sprites = RetornaLista();
+        Render = GetComponent<Image>();
+    }
+
+   
+    void Update()
+    {
+
+        index += Time.deltaTime * velocidade;
+        index = index > sprites.Count ? 0 : index;
+
+        Render.sprite = sprites[(int)index];
+    }
+
+    List<Sprite> RetornaLista()
+    {
+        List<Sprite> Animacoes = new List<Sprite>();
+
+        for (int i = 0; i < 129; i++)
+        {
+            Animacoes.Add(Resources.Load<Sprite>("frame_" + i.ToString("000") + "_delay-0.04s"));
+        }
+
+        return Animacoes;
+    }
+
+}
